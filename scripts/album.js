0149bff2fc02ec95ae09840a2120ccb13536f4c3
Julie Park <jspark721@gmail.com>
2016-07-14 16:35:34 -0500

    Assignment 27 completed

--- scripts/album.js
+++ scripts/album.js
@@ -61,12 +61,19 @@ var setCurrentAlbum = function(album) {
  };
 
 var findParentByClassName = function(element, targetClass) {
-    if (element) {
-        var currentParent = element.parentElement;
-        while (currentParent.className != targetClass && currentParent.className !== null) {
+    var currentParent = element.parentElement;
+    if (currentParent) {
+        while (currentParent.className != targetClass) {
             currentParent = currentParent.parentElement;
         }
-        return currentParent;
+        
+        if (currentParent.className == targetClass) {
+            return currentParent;
+        } else {
+            alert("No parent with that class name found.");
+        }
+    } else {
+        alert("No parent found");
     }
 };
 
